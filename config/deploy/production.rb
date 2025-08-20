325181e51592b8207c612c669d32ea30806f6d69
ihortok <itokaryk@gmail.com>
2025-08-21 00:19:52 +0200

    new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '194.99.21.141', user: 'deploy', roles: %w[app db web]
+server '89.38.135.187', user: 'deploy', roles: %w[app db web]
 
 set :deploy_to, '/home/deploy/events'
 set :branch, 'main'
